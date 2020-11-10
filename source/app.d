b35e6a18d480c591b93dbb4ec03dadce847d4e20
Dmitry Kalinin <mitya17092001@gmail.com>
2020-11-10 17:51:45 +0300

    redesigned point

--- source/app.d
+++ source/app.d
@@ -1,37 +1,38 @@
 import std.stdio;
 
-import std.algorithm : reduce;
+import std.algorithm : each;
+import std.range;
 import std.array : array;
 import std.math : exp, pow;
 import std.random : uniform;
+import std.typecons;
 
 alias fn = (x, y) => exp(-pow(x, 2) - pow(y, 2));
 
 immutable ulong N = 6u;
 
-struct Point {
-    private double[3] arr;
-    alias arr this;
+alias Point = Tuple!(double, "x", double, "y", double, "fit");
 
-    static Point generate(alias func)() {
-        Point p;
-        p[0] = uniform!"[]"(-2.0, 2.0);
-        p[1] = uniform!"[]"(-2.0, 2.0);
-        p[2] = func(p[0], p[1]);
-        return p;
-    }
+void generatePoint(alias func)(ref Point p) {
+    p.x = uniform!"()"(-2.0, 2.0);
+    p.y = uniform!"()"(-2.0, 2.0);
+    p.fit = func(p.x, p.y);
 }
-
 
 void main() {
     Point[] arr = new Point[] (N);
-    Selection[] s = new Selection[] (N)
 
-    foreach(ref elem; arr) {
-        elem = Point.generate!fn();
-    }
+    arr.each!(generatePoint!fn);
 
+    alias select2 = (arr) {
+        ulong i1, i2;
+        while (i1 != i2) {
+            i1 = uniform(0, arr.length);
+            i2 = uniform(0, arr.length);
+        }
+        return [arr[i1], arr[i2]];
+    };
     for (ulong i = 0; i != 10; ++i) {
+        auto pair = select2(arr);
     }
-
 }
